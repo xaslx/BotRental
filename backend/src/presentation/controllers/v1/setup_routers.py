0ee6582fc4230b68e544d52d2b6f59d82d56b91b
maksim <knyazev.maksim2852@gmail.com>
2025-06-08 16:12:22 +0400

    register new route

--- backend/src/presentation/controllers/v1/setup_routers.py
+++ backend/src/presentation/controllers/v1/setup_routers.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from src.presentation.controllers.v1.auth import router as auth_router
 from src.presentation.controllers.v1.monitoring import router as monitoring_router
+from src.presentation.controllers.v1.user import router as user_router
+
+
 
 def setup_controllers(app: FastAPI):
 
 
     app.include_router(auth_router, prefix='/api/v1', tags=['Авторизация и Аутентификация'])
     app.include_router(monitoring_router, prefix='/api/v1', tags=['Мониторинг'])
+    app.include_router(user_router, prefix='/api/v1/users', tags=['Пользователи'])
