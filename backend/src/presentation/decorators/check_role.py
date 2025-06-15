96a830cb0a124cd646159285ad8c073f7c6a5904
maksim <knyazev.maksim2852@gmail.com>
2025-06-15 18:03:31 +0400

    add check_role decorator

---
+++ backend/src/presentation/decorators/check_role.py
@@ -0,0 +1,17 @@
+from functools import wraps
+from src.domain.user.exception import PermissionDeniedException
+from src.domain.user.entity import UserEntity
+
+
+
+def check_role(allowed_roles: list[str]):
+    def decorator(func):
+        @wraps(func)
+        async def wrapper(*args, **kwargs):
+            user: UserEntity | None = kwargs.get('user')
+
+            if not user or user.role not in allowed_roles:
+                raise PermissionDeniedException()
+            return await func(*args, **kwargs)
+        return wrapper
+    return decorator
