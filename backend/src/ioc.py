3a4809e2cc91c1d94d3465664c15ac971eda7c60
maksim <knyazev.maksim2852@gmail.com>
2025-06-15 17:58:43 +0400

    add new use cases

--- backend/src/ioc.py
+++ backend/src/ioc.py
@@ -1,15 +1,21 @@
-from dishka import Provider, Scope, provide, from_context
-from fastapi import Request
+from dishka import Provider, Scope, provide, from_context, decorate
+from fastapi import HTTPException, Request, status
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 from typing import AsyncIterable
+from src.application.use_cases.admin.users.get_users import GetAllUsersUseCase, GetUserByTelegramId
+from src.domain.user.entity import UserEntity
+from src.application.services.auth import AuthServiceImpl, BaseAuthService
+from src.application.services.jwt import JWTService, JWTServiceImpl
 from src.config import Config
 from src.infrastructure.database.postgresql import new_session_maker
-from src.application.use_cases.user.register import NewUserUseCase, RegisterUserUseCase
+from src.application.use_cases.user.auth import RefreshTokenUseCase, RegisterUserUseCase, LoginUserUseCase, VerifyCodeUseCase, SendCodeUseCase
 from src.infrastructure.repositories.user.base import BaseUserRepository
 from src.infrastructure.repositories.user.sqlalchemy import SQLAlchemyUserRepository
 from src.infrastructure.cache.base import BaseCacheService
 from src.infrastructure.cache.redis import RedisCacheService
+from src.presentation.schemas.jwt_token import AccessTokenReponse
 from src.application.services.code import CheckCodeService, SendCodeService
+from src.domain.jwt.exception import TokenAbsentException
 
 
 class AppProvider(Provider):
@@ -36,30 +42,74 @@ class AppProvider(Provider):
 
     #USE CASES
     @provide(scope=Scope.REQUEST)
-    def get_new_user_use_case(
+    def get_send_code_use_case(
         self,
-        cache_service: BaseCacheService,
         send_code_service: SendCodeService,
-        user_repository: BaseUserRepository,
-    ) -> NewUserUseCase:
+    ) -> SendCodeUseCase:
         
-        return NewUserUseCase(
-            user_repository=user_repository,
-            send_code_service=send_code_service,
-            cache_service=cache_service,
+        return SendCodeUseCase(_send_code_service=send_code_service)
+
+    @provide(scope=Scope.REQUEST)
+    def get_verify_code_use_case(
+        self,
+        check_code_service: CheckCodeService,
+        login_user_use_case: LoginUserUseCase,
+        register_user_use_case: RegisterUserUseCase,
+        user_repository: BaseUserRepository,
+    ) -> VerifyCodeUseCase:
+        
+        return VerifyCodeUseCase(
+            _check_code=check_code_service,
+            _login_use_case=login_user_use_case,
+            _register_use_case=register_user_use_case,
+            _user_repository=user_repository,
         )
+
+    @provide(scope=Scope.REQUEST)
+    def get_refresh_token_use_case(
+        self,
+        jwt_service: JWTService,
+    ) -> RefreshTokenUseCase:
+        
+        return RefreshTokenUseCase(_jwt_service=jwt_service)
 
     @provide(scope=Scope.REQUEST)
     def get_register_user_use_case(
         self,
-        check_code: CheckCodeService,
         user_repository: BaseUserRepository,
+        jwt_service: JWTService,
     ) -> RegisterUserUseCase:
         
         return RegisterUserUseCase(
-            check_code=check_code,
-            user_repository=user_repository,
+            _user_repository=user_repository,
+            _jwt_service=jwt_service,
         )
+    
+    @provide(scope=Scope.REQUEST)
+    def get_login_user_use_case(
+        self,
+        user_repository: BaseUserRepository,
+        jwt_service: JWTService,
+    ) -> LoginUserUseCase:
+        
+        return LoginUserUseCase(_user_repository=user_repository, _jwt_service=jwt_service)
+    
+    @provide(scope=Scope.REQUEST)
+    def get_all_users_use_case(
+        self,
+        user_repository: BaseUserRepository,
+    ) -> GetAllUsersUseCase:
+        
+        return GetAllUsersUseCase(_user_repository=user_repository)
+    
+    @provide(scope=Scope.REQUEST)
+    def get_users_by_telegram_id_use_case(
+        self,
+        user_repository: BaseUserRepository,
+    ) -> GetUserByTelegramId:
+        
+        return GetUserByTelegramId(_user_repository=user_repository)
+
 
     #SERVICES
     @provide(scope=Scope.REQUEST)
@@ -70,13 +120,20 @@ class AppProvider(Provider):
         return RedisCacheService()
     
     @provide(scope=Scope.REQUEST)
+    def get_jwt_service(
+        self,
+        config: Config,
+    ) -> JWTService:
+        
+        return JWTServiceImpl(config=config)
+    
+    @provide(scope=Scope.REQUEST)
     def get_send_code_service(
         self,
-        user_repository: BaseUserRepository,
         cache_service: BaseCacheService,
     ) -> SendCodeService:
         
-        return SendCodeService(user_repository=user_repository, cache_service=cache_service)
+        return SendCodeService(_cache_service=cache_service)
         
     @provide(scope=Scope.REQUEST)
     def get_check_code_service(
@@ -85,28 +142,43 @@ class AppProvider(Provider):
     ) -> CheckCodeService:
         
         return CheckCodeService(
-            cache_service=cache_service,
+            _cache_service=cache_service,
         )
 
-    # #current user
-    # @provide(scope=Scope.REQUEST)
-    # def get_token(self, request: Request) -> str:
+    @provide(scope=Scope.REQUEST)
+    def get_auth_service(
+        self,
+        user_repository: BaseUserRepository,
+        jwt_service: JWTService,
+        cache_service: BaseCacheService,
+    ) -> BaseAuthService:
         
-    #     token: str = request.cookies.get('user_access_token')
+        return AuthServiceImpl(
+            _user_repository=user_repository,
+            _jwt_service=jwt_service,
+            _cache_service=cache_service,
+        )
+
+    #current user
+    @provide(scope=Scope.REQUEST)
+    def get_access_token(self, request: Request) -> AccessTokenReponse:
+        token: str | None = request.cookies.get('access_token')
+        if not token:
+            raise TokenAbsentException()
+        return AccessTokenReponse(token=token)
+
     
-    #     if not token:
-    #         return None
-    #     return token
-
-
-    # @provide(scope=Scope.REQUEST)
-    # async def get_current_user_dependency(
-    #     self,
-    #     auth_service: BaseAuthService,
-    #     token: str,
-    # ) -> UserEntity:
-
-    #     if not token:
-    #         return None
+    @provide(scope=Scope.REQUEST)
+    async def get_current_user(
+        self,
+        auth_service: BaseAuthService,
+        access_token: AccessTokenReponse,
+    ) -> UserEntity:
         
-    #     return await auth_service.get_current_user(token=token)
+        if not access_token:
+            return None
+
+        user: UserEntity | None = await auth_service.get_current_user(token=access_token.token)
+        if not user:
+            return None
+        return user
