44ac382c3863c5cc08011f5137f47ea17a7b61e2
maksim <knyazev.maksim2852@gmail.com>
2025-06-19 16:39:22 +0400

    add delete_user use case

---
+++ backend/src/application/use_cases/admin/users/delete_user.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from src.domain.user.entity import UserEntity
+from src.domain.user.exception import UserNotFoundException, PermissionDeniedException
+from src.infrastructure.repositories.user.base import BaseUserRepository
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class DeleteUserUseCase:
+    _user_repository: BaseUserRepository
+
+    async def execute(self, telegram_id: int, admin: UserEntity) -> UserEntity:
+
+        user: UserEntity | None = await self._user_repository.get_user_by_telegram_id(telegram_id=telegram_id)
+        
+
+        if not user:
+            raise UserNotFoundException()
+        
+        
+        if user.telegram_id.to_raw() == admin.telegram_id.to_raw():
+            logger.warning(f'Администратор: {admin.telegram_id.to_raw()} попытался удалить сам себя')
+            raise PermissionDeniedException()
+        
+        if user.role in ['dev', 'admin']:
+            logger.warning(
+                f'Администратор: {admin.telegram_id.to_raw()} попытался удалить администратора: {user.telegram_id.to_raw()}'
+            )
+            raise PermissionDeniedException()
+
+        if not user.is_deleted:
+            user.delete()
+            deleted_user: UserEntity = await self._user_repository.update(entity=user)
+            logger.info(f'Администратор: {admin.telegram_id.to_raw()} удалил пользователя: {user.telegram_id.to_raw()}')
+
+        return deleted_user
