29553bed00245978999a03524219a0540583a019
maksim <knyazev.maksim2852@gmail.com>
2025-06-20 16:12:55 +0400

    include new routers

--- backend/src/presentation/controllers/v1/setup_routers.py
+++ backend/src/presentation/controllers/v1/setup_routers.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
-from src.presentation.controllers.v1.auth import router as auth_router
-from src.presentation.controllers.v1.monitoring import router as monitoring_router
-from src.presentation.controllers.v1.user import router as user_router
-
+from src.presentation.controllers.v1.users.auth import router as auth_router
+from src.presentation.controllers.v1.admins.monitoring import router as admins_monitoring_router
+from src.presentation.controllers.v1.users.users import router as user_router
+from src.presentation.controllers.v1.admins.users import router as admins_users_router
 
 
 def setup_controllers(app: FastAPI):
 
 
     app.include_router(auth_router, prefix='/api/v1', tags=['Авторизация и Аутентификация'])
-    app.include_router(monitoring_router, prefix='/api/v1', tags=['Мониторинг'])
+    app.include_router(admins_monitoring_router, prefix='/api/v1/admins', tags=['Мониторинг Админ'])
     app.include_router(user_router, prefix='/api/v1/users', tags=['Пользователи'])
+    app.include_router(admins_users_router, prefix='/api/v1/admins', tags=['Пользователи Админ'])
