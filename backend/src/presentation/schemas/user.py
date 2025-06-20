a64f693c36cb2b58ab4e6dfe42ebed72f69e59ac
maksim <knyazev.maksim2852@gmail.com>
2025-06-20 16:11:58 +0400

    add new user schemas

--- backend/src/presentation/schemas/user.py
+++ backend/src/presentation/schemas/user.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from src.domain.user.value_object import TelegramId
 from src.domain.balance.value_object import Balance
 from src.domain.user.entity import Role
+from datetime import datetime
 
 
 class CheckCodeSchema(BaseModel):
@@ -19,6 +20,32 @@ class UserOutSchema(BaseModel):
     created_at: datetime
     telegram_id: TelegramId
     balance: Balance
+    blocks: list
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UserAdminViewSchema(UserOutSchema):
+    is_deleted: bool
     role: Role
+    
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UpdateUserRole(BaseModel):
+    role: Role
+
+
+class UserBlockSchema(BaseModel):
+    reason: str
+    days: int
+
+
+class BlockedUserOutSchema(BaseModel):
+    created_at: datetime
+    user_id: int
+    blocked_until: datetime
+    reason: str
+    blocked_by: int
 
     model_config = ConfigDict(from_attributes=True)
