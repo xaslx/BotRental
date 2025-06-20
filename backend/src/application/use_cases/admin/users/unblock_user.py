312306b55afc4671bb06b3d88e88344bd67648aa
maksim <knyazev.maksim2852@gmail.com>
2025-06-20 16:01:06 +0400

    add unblock user use case

---
+++ backend/src/application/use_cases/admin/users/unblock_user.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+import logging
+from src.domain.user.blocked_user import BlockedUserEntity
+from src.domain.user.exception import UserNotFoundException
+from src.domain.user.entity import UserEntity
+from src.infrastructure.repositories.user.base import BaseBlockedUserRepository, BaseUserRepository
+
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class UnblockUserUseCase:
+    _user_repository: BaseUserRepository
+    _blocked_user_repository: BaseBlockedUserRepository
+
+    async def execute(self, telegram_id: int, admin: UserEntity) -> bool:
+    
+        user: UserEntity | None = await self._user_repository.get_user_by_telegram_id(telegram_id=telegram_id)
+
+        if not user:
+            raise UserNotFoundException()
+        
+        block: BlockedUserEntity = user.unblock()
+
+        await self._blocked_user_repository.update(block=block)
+        logger.info(f'Администратор: {admin.telegram_id.to_raw()} разблокировал пользователя: {user.telegram_id.to_raw()}')
+        return True
+
+
