4b573078c4b477f9d6f8e65e9be4ee6ed91b6552
maksim <knyazev.maksim2852@gmail.com>
2025-06-23 20:55:07 +0400

    add new exception for bot

--- backend/src/domain/bot/exception.py
+++ backend/src/domain/bot/exception.py
@@ -19,3 +19,39 @@ class InvalidLengthException(DomainErrorException):
     @property
     def message(self) -> str:
         return 'Поле не должно быть пустым.'
+    
+
+@dataclass(eq=False)
+class BotNotFoundException(DomainErrorException):
+    status_code: int = status.HTTP_404_NOT_FOUND
+
+    @property
+    def message(self) -> str:
+        return 'Бот не найден.'
+    
+
+@dataclass(eq=False)
+class BotAlreadyDeletedException(DomainErrorException):
+    status_code: int = status.HTTP_400_BAD_REQUEST
+
+    @property
+    def message(self) -> str:
+        return 'Бот уже удален.'
+    
+
+@dataclass(eq=False)
+class BotAlreadyActivatedException(DomainErrorException):
+    status_code: int = status.HTTP_400_BAD_REQUEST
+
+    @property
+    def message(self) -> str:
+        return 'Бот уже активирован.'
+    
+
+@dataclass(eq=False)
+class BotAlreadyDeactivatedException(DomainErrorException):
+    status_code: int = status.HTTP_400_BAD_REQUEST
+
+    @property
+    def message(self) -> str:
+        return 'Бот уже деактивирован.'
