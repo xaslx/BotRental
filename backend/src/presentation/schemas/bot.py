3d27c64e7ca665fde9d8d9134ed15d56e2925c3b
maksim <knyazev.maksim2852@gmail.com>
2025-06-23 20:46:13 +0400

    add bot schemas

---
+++ backend/src/presentation/schemas/bot.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+from pydantic import ConfigDict, BaseModel
+from src.presentation.schemas.user import UserOutSchema
+
+
+class CreateBotSchema(BaseModel):
+    name: str
+    description: str
+    price: int
+
+
+class UpdateBotSchema(BaseModel):
+    name: str | None = None
+    description: str | None = None
+    price: int | None = None
+
+
+class BotOutSchema(CreateBotSchema):
+    id: int
+    is_available: bool
+    is_deleted: bool
+    created_at: datetime
+    updated_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+
+class BotRentalOutSchema(BaseModel):
+    id: int
+    user_id: int
+    bot_id: int
+    token: str
+    rented_until: datetime
+    is_active: bool
+    user: UserOutSchema
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class BotAdminOutSchema(BotOutSchema):
+    rentals: list[BotRentalOutSchema] = []
+
+    model_config = ConfigDict(from_attributes=True)
