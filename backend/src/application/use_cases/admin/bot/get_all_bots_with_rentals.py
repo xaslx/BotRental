494aab8057b1be2200380398de682fe1a010ac8f
maksim <knyazev.maksim2852@gmail.com>
2025-06-23 21:15:16 +0400

    add get_all_bots_with_rentals use case

---
+++ backend/src/application/use_cases/admin/bot/get_all_bots_with_rentals.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+from src.domain.bot.entity import BotEntity
+from src.infrastructure.repositories.bot.base import BaseBotRepository
+
+
+@dataclass
+class GetAllBotsWithRentalsUseCase:
+
+    _bot_repository: BaseBotRepository
+
+    async def execute(self) -> list[BotEntity] | None:
+        
+        return await self._bot_repository.get_all_bots_with_rentals()
+    
