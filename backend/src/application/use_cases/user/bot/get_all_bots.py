44cc7ffd2b5045aa0e017e02297b356f356376d7
maksim <knyazev.maksim2852@gmail.com>
2025-06-23 20:56:40 +0400

    add user - bot use case

---
+++ backend/src/application/use_cases/user/bot/get_all_bots.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+from src.domain.bot.entity import BotEntity
+from src.infrastructure.repositories.bot.base import BaseBotRepository
+
+
+@dataclass
+class GetAllBotsUseCase:
+
+    _bot_repository: BaseBotRepository
+
+    async def execute(self) -> list[BotEntity] | None:
+        
+        return await self._bot_repository.get_all_bots()
