f3270709f588db1e2b8bb7ff5829e114aaa915bd
maksim <knyazev.maksim2852@gmail.com>
2025-06-29 17:02:24 +0400

    add new schemas for user

--- backend/src/presentation/schemas/user.py
+++ backend/src/presentation/schemas/user.py
@@ -1,7 +1,5 @@
 from pydantic import BaseModel, ConfigDict, Field
 from datetime import datetime
-from src.domain.user.value_object import TelegramId
-from src.domain.balance.value_object import Balance
 from src.domain.user.entity import Role
 from datetime import datetime
 
@@ -13,32 +11,7 @@ class CheckCodeSchema(BaseModel):
 
 class SendCodeSchema(BaseModel):
     telegram_id: int = Field(gt=0)
-
-
-class UserOutSchema(BaseModel):
-    id: int
-    created_at: datetime
-    telegram_id: int
-    balance: int
-    blocks: list
-
-    model_config = ConfigDict(from_attributes=True)
-
-
-class UserAdminViewSchema(UserOutSchema):
-    is_deleted: bool
-    role: Role
-    
-    model_config = ConfigDict(from_attributes=True)
-
-
-class UpdateUserRole(BaseModel):
-    role: Role
-
-
-class UserBlockSchema(BaseModel):
-    reason: str
-    days: int
+    ref_id: int | None = None
 
 
 class BlockedUserOutSchema(BaseModel):
@@ -49,3 +22,56 @@ class BlockedUserOutSchema(BaseModel):
     blocked_by: int
 
     model_config = ConfigDict(from_attributes=True)
+
+
+class BotRentalOutSchema(BaseModel):
+    id: int
+    bot_id: int
+    user_id: int
+    rented_until: datetime
+    token: str
+    rented_until: datetime
+    is_active: bool
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class ReferralOutSchema(BaseModel):
+    id: int | None
+    referrer_id: int
+    referral_id: int
+    telegram_id: int
+    invited_at: datetime
+    total_bonus: int
+
+    model_config = ConfigDict(from_attributes=True)
+
+class UserOutSchema(BaseModel):
+    id: int
+    created_at: datetime
+    telegram_id: int
+    balance: int
+    blocks: list[BlockedUserOutSchema]
+    referrer_id: int | None
+    total_bonus_received: int
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UserAdminViewSchema(UserOutSchema):
+    is_deleted: bool
+    role: Role
+    rentals: list[BotRentalOutSchema] | None
+    referrals: list[ReferralOutSchema] | None
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+
+class UpdateUserRole(BaseModel):
+    role: Role
+
+
+class UserBlockSchema(BaseModel):
+    reason: str
+    days: int
