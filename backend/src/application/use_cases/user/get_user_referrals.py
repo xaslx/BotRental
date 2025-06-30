8a66f33586aa5b8b4b46a3b173a89886f7a35b79
maksim <knyazev.maksim2852@gmail.com>
2025-06-30 17:09:43 +0400

    add new use case for user

---
+++ backend/src/application/use_cases/user/get_user_referrals.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+from src.domain.referral.entity import ReferralEntity
+from src.infrastructure.repositories.referral.base import BaseReferralRepository
+
+
+@dataclass
+class GetUserReferralsUseCase:
+    _referral_repository: BaseReferralRepository
+
+    async def execute(self, referrer_id: int) -> list[ReferralEntity]:
+        
+        return await self._referral_repository.get_all_referrals_by_referrer_id(
+            referrer_id=referrer_id,
+        )
