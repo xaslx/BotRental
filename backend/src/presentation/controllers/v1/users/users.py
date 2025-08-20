442e9033f762dd3548ed1cc72ae3062e0a321ae1
maksim <knyazev.maksim2852@gmail.com>
2025-08-20 17:49:38 +0400

    add new users endpoints

--- backend/src/presentation/controllers/v1/users/users.py
+++ backend/src/presentation/controllers/v1/users/users.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter, status
 from dishka.integrations.fastapi import inject, FromDishka as Depends
+from src.domain.bot.entity import BotRentalEntity
+from src.application.use_cases.user.get_user_rentals import GetUserRentalsUseCase
+from src.domain.referral.entity import ReferralEntity
+from src.application.use_cases.user.get_user_referrals import GetUserReferralsUseCase
 from src.presentation.schemas.error import ErrorSchema
-from src.presentation.schemas.user import UserOutSchema
+from src.presentation.schemas.user import BlockedUserOutSchema, ReferralOutSchema, UserOutSchema, BotRentalOutSchema
 from src.domain.user.entity import UserEntity
 
 
@@ -9,7 +13,7 @@ router: APIRouter = APIRouter()
 
 
 @router.get(
-    '/me',
+    '/profile',
     description='Эндпоинт для отображения профиля пользователя',
     status_code=status.HTTP_200_OK,
     responses={
@@ -23,3 +27,69 @@ async def get_my_profile(
 ) -> UserOutSchema | None:
     
     return UserOutSchema.model_validate(user.to_dict())
+
+
+@router.get(
+    '/profile/blocks',
+    description='Эндпоинт для отображения всех блокировок пользователя',
+    status_code=status.HTTP_200_OK,
+    responses={
+        status.HTTP_200_OK: {
+            'model': list[BlockedUserOutSchema],
+            'description': 'Список блокировок пользователя',
+        },
+        status.HTTP_401_UNAUTHORIZED: {'model': ErrorSchema, 'description': 'Пользователь не аутентифицирован'},
+    },
+
+)
+@inject
+async def get_my_block(
+    user: Depends[UserEntity],
+) -> list[BlockedUserOutSchema]:
+    
+    return [BlockedUserOutSchema.model_validate(block) for block in user.blocks]
+
+
+@router.get(
+    '/profile/referrals',
+    description='Эндпоинт для отображения всех рефералов пользователя',
+    status_code=status.HTTP_200_OK,
+    responses={
+        status.HTTP_200_OK: {
+            'model': list[ReferralOutSchema],
+            'description': 'Список рефералов пользователя',
+        },
+        status.HTTP_401_UNAUTHORIZED: {'model': ErrorSchema, 'description': 'Пользователь не аутентифицирован'},
+    },
+)
+@inject
+async def get_my_referrals(
+    user: Depends[UserEntity],
+    use_case: Depends[GetUserReferralsUseCase],
+) -> list[ReferralOutSchema]:
+    
+    referrals: list[ReferralEntity] = await use_case.execute(referrer_id=user.id)
+    return [ReferralOutSchema.model_validate(referral.to_dict()) for referral in referrals]
+
+
+
+@router.get(
+    '/profile/rentals',
+    description='Эндпоинт для отображения всех аренд ботов у пользователя',
+    status_code=status.HTTP_200_OK,
+    responses={
+        status.HTTP_200_OK: {
+            'model': list[BotRentalOutSchema],
+            'description': 'Список аренд пользователя',
+        },
+        status.HTTP_401_UNAUTHORIZED: {'model': ErrorSchema, 'description': 'Пользователь не аутентифицирован'},
+    },
+)
+@inject
+async def get_my_rentals(
+    user: Depends[UserEntity],
+    use_case: Depends[GetUserRentalsUseCase],
+) -> list[BotRentalOutSchema]:
+    
+    rentals: list[BotRentalEntity] = await use_case.execute(user_id=user.id)
+    return [BotRentalOutSchema.model_validate(rent) for rent in rentals]
