84fb480e4441bad354a067c9c9f75fb0b4fc579a
maksim <knyazev.maksim2852@gmail.com>
2025-08-20 19:52:11 +0400

    add stop bot use case

---
+++ backend/src/application/use_cases/user/bot/stop_bot.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+from src.domain.bot.exception import RentalNotFoundException
+from src.domain.bot.entity import BotRentalEntity
+from src.domain.user.exception import PermissionDeniedException
+from src.infrastructure.repositories.rental.base import BaseRentalRepository
+from src.domain.user.entity import UserEntity
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+@dataclass
+class StopBotRentalUseCase:
+    _rental_repository: BaseRentalRepository
+    _session: AsyncSession
+
+    async def execute(self, rental_id: int, user: UserEntity) -> bool:
+
+        rental: BotRentalEntity | None = await self._rental_repository.get_by_id(rental_id=rental_id)
+
+        if not rental:
+            raise RentalNotFoundException()
+
+        if rental.user_id != user.id:
+            raise PermissionDeniedException()
+        
+        rental.stop()
+
+        await self._rental_repository.update(rental=rental)
+
+        await self._session.commit()
+
+        return True
+
